637683a1bd1bcee3f4677034adf2f4380a7e75f1
Piter1608 <108742186+Piter1608@users.noreply.github.com>
2023-08-04 13:17:25 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,11 +15,6 @@ resource "github_branch_protection" "main" {
   repository_id = "github-terraform-task-Piter1608"
   pattern     = "main"
 
-  required_pull_request_reviews {
-    dismiss_stale_reviews = false
-    required_approving_review_count = 2
-  }
-
   enforce_admins = true
 }
 resource "github_branch" "develop" {
